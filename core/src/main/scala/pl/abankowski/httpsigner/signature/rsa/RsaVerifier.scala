c1c6cbda5fb00198cc7ce70637a8e31b84e376e7
Artur Bańkowski <artur.bankowski@gmail.com>
2020-03-26 17:13:33 +0100

    Provide some boilerplate classes for separating signature keys

--- core/src/main/scala/pl/abankowski/httpsigner/signature/rsa/RsaVerifier.scala
+++ core/src/main/scala/pl/abankowski/httpsigner/signature/rsa/RsaVerifier.scala
@@ -3,4 +3,8 @@ package pl.abankowski.httpsigner.signature.rsa
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter
 import pl.abankowski.httpsigner.signature.RsaSHA512Verifier
 
-class RsaVerifier(override val pubKey: AsymmetricKeyParameter) extends RsaSHA512Verifier
+final class RsaVerifier(override val pubKey: AsymmetricKeyParameter) extends RsaSHA512Verifier
+
+object RsaVerifier {
+  def apply(pub: AsymmetricKeyParameter) = new RsaVerifier(pub)
+}
