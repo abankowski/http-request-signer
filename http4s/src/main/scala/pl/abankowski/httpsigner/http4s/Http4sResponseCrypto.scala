d3716bacfb32ff43c85f51d25251fd67fb0ce2ad
Marcin Wadoń <mwadon@evojam.com>
2020-04-02 23:42:48 +0200

    Use polymorphic type instead of IO for http4s

--- http4s/src/main/scala/pl/abankowski/httpsigner/http4s/Http4sResponseCrypto.scala
+++ http4s/src/main/scala/pl/abankowski/httpsigner/http4s/Http4sResponseCrypto.scala
@@ -1,21 +1,24 @@
 package pl.abankowski.httpsigner.http4s
 
-import cats.effect.{ContextShift, IO}
+import cats.effect.{ContextShift, Sync}
 import pl.abankowski.httpsigner.signature.{Generator, Verifier}
 import pl.abankowski.httpsigner.HttpCryptoConfig
 
-class Http4sResponseSigner(
+class Http4sResponseSigner[F[_]](
   override val crypto: Generator,
-  override val config: HttpCryptoConfig = new HttpCryptoConfig {})(
-  override implicit val ctx: ContextShift[IO]) extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseSigner
+  override val config: HttpCryptoConfig = new HttpCryptoConfig {}
+)(override implicit val ctx: ContextShift[F], override implicit val F: Sync[F])
+    extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseSigner[F]
 
-class Http4sResponseVerifier(
+class Http4sResponseVerifier[F[_]](
   override val crypto: Verifier,
-  override val config: HttpCryptoConfig = new HttpCryptoConfig {})(
-  override implicit val ctx: ContextShift[IO]) extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseVerifier
+  override val config: HttpCryptoConfig = new HttpCryptoConfig {}
+)(override implicit val ctx: ContextShift[F], override implicit val F: Sync[F])
+    extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseVerifier[F]
 
-class Http4SResponseCrypto(
+class Http4SResponseCrypto[F[_]](
   override val crypto: Generator with Verifier,
-  override val config: HttpCryptoConfig = new HttpCryptoConfig {})(override implicit val ctx: ContextShift[IO])
-  extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseSigner
-    with pl.abankowski.httpsigner.http4s.impl.Http4sResponseVerifier
+  override val config: HttpCryptoConfig = new HttpCryptoConfig {}
+)(override implicit val ctx: ContextShift[F], override implicit val F: Sync[F])
+    extends pl.abankowski.httpsigner.http4s.impl.Http4sResponseSigner[F]
+    with pl.abankowski.httpsigner.http4s.impl.Http4sResponseVerifier[F]
